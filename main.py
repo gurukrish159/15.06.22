9afef76f83fe118824d4062eb9253a3850441bcb
gurukrish159 <gurukrishna159@gmail.com>
2022-06-20 17:40:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+li=list(input().strip())
+start=0
+for i in range(len(li)):
+    if li[i] in "aeiou":
+        for j in range(i,start-1,-1):
+            if li[j] not in "aeiou":
+                li[i]=li[j]
+                start=j
+                break
+print(*li,sep="")
